4f4d37f9823ab30a47924d17e6ac3639afdb2fb4
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-07-29 11:36:32 +0530

    DB connected successfully

---
+++ src/index.js
@@ -0,0 +1,80 @@
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+
+;(async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (err) => {
+            console.log("Error occurred while listening to the server : ", err);
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    }catch(err){
+        console.error("ERROR : ",err);
+        throw err;
+    }
+})()
+
+*/
+
+
