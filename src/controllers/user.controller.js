ed46430b46b3115c2b43e3d710fa892fd30be18e
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-08-04 11:18:16 +0530

    added refreshAccessToken endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import  { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
 
 
 const generateAccessAndRefreshTokens = async ( userId ) => {
@@ -36,6 +37,7 @@ const registerUser = asyncHandler( async ( req, res ) => {
     
     const avatarLocalPath = req.files?.avatar[0]?.path;
     // const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+    // console.log(req.files);
 
     let coverImageLocalPath;
     if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
@@ -54,11 +56,11 @@ const registerUser = asyncHandler( async ( req, res ) => {
 
     const user = await User.create({
         fullName,
-        avatar : avatar.url,
-        coverImage : coverImage?.url || " ",
-        email,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email, 
         password,
-        username : username.toLowerCase()
+        username: username.toLowerCase()
     })
 
     const createdUser = await User.findById(user._id).select(
@@ -78,8 +80,9 @@ const registerUser = asyncHandler( async ( req, res ) => {
 const loginUser = asyncHandler( async ( req, res ) => {
 
     const { email, username, password } = req.body;
+    // console.log(email, username, password);
 
-    if(!username || !email ){
+    if(!username && !email ){
         throw new ApiError(400,"Username or email is required");
     }
 
@@ -99,7 +102,8 @@ const loginUser = asyncHandler( async ( req, res ) => {
 
     const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
 
-    const loggedInUser = User.findById(user._id).select("-password -refreshToken");
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+    // console.log("LoggedInUser : ",loggedInUser);
 
     const options = {
         httpOnly : true,
@@ -142,12 +146,58 @@ const logoutUser = asyncHandler( async ( req, res ) => {
     .json(new ApiResponse(200, {}, "User Logged Out"))
 })
 
+const refreshAccesstoken = asyncHandler( async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+
+    if(!incomingRefreshToken){
+        throw new ApiError(401, "Unauthorized Request");
+    }
+
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+    
+        const user = await User.findById(decodedToken?._id);
+    
+        if(!user){
+            throw new ApiError(401, " Invalid refresh Token");
+        }
+        
+        if(incomingRefreshToken != user.refreshToken){
+            throw new ApiError(401, "Refresh token is expired or used");
+        }
+    
+        const options = {
+            httpOnly : true,
+            secure : true
+        }
+        const {accessToken, newRefreshToken} = await generateAccessAndRefreshTokens(user._id);
+    
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken )
+        .cookie("refreshToken", newRefreshToken)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    accessToken, refreshToken : newRefreshToken
+                },
+                "Access Token refreshed"
+            )
+        )
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid refresh token");
+    }
+    
+})
+
 
 
 export { 
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccesstoken
 }
 
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
-
-
 cloudinary.config({
     cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
     api_key : process.env.CLOUDINARY_API_KEY,
@@ -14,17 +12,22 @@ cloudinary.config({
 const uploadOnCloudinary = async ( localFilePath ) => {
     try{
         if(!localFilePath){
+            // console.log("Returned null");
             return null;
         }
+        // console.log(localFilePath);
         // uploading the file on cloudinary
-
+        // console.log("Till thissss");
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type : "auto"
+        }).catch((error) => {
+            console.log("Cloudinary error : ",error);
         })
-
+        // console.log("Till this");
         // after successfully uploading of the file
 
         // console.log("File is uploaded on cloudinary : ", response.url);
+        // console.log("Response : ", response);
         fs.unlinkSync(localFilePath);
         return response;
 
