af05655237778670042f9b6808cab7f4c20b712b
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-08-11 13:53:49 +0530

    removed redundant imports

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -81,10 +81,6 @@ const getChannelStats = asyncHandler(async (req, res) => {
     }
 });
 
-import { Video } from "../models/video.model.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     try {
