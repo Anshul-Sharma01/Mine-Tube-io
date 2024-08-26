c7812644133a53d5581f6d640a037c3265248d24
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-08-26 09:42:41 +0530

    added like routes and controller structure

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,30 @@
+import { asyncHandler } from "../utils/asyncHandler";
+
+
+
+const toggleVideoLike = asyncHandler(async (req, res, next) => {
+
+})
+
+const toggleCommentLike = asyncHandler(async ( req, res, next) => {
+
+})
+
+const toggleTweetLike = asyncHandler(async(req, res, next) => {
+
+})
+
+const getLikedVideos = asyncHandler(async(req, res, next) => {
+
+})
+
+
+export { 
+    toggleCommentLike,
+    toggleTweetLike,
+    toggleVideoLike,
+    getLikedVideos
+}
+
+
+
