da1f1a577700e0845f69328548736e525d549263
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-08-27 14:33:16 +0530

    Added playlist routes and controller structure

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ import dashboardRouter from "./routes/dashboard.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
-
+import playlistRouter from "./routes/playlist.routes.js";
 
 // routes declaration
 app.use("/api/v1/users", userRoutes);
@@ -33,6 +33,7 @@ app.use("api/v1/dashboard", dashboardRouter);
 app.use("api/v1/videos", videoRouter);
 app.use("api/v1/comments", commentRouter);
 app.use("/api/v1/like", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
 
 
 export { app };
---
+++ src/controllers/playlist.controller.js
@@ -0,0 +1,43 @@
+import { asyncHandler } from "../utils/asyncHandler";
+
+
+const createPlaylist = asyncHandler(async(req, res, next) => {
+
+})
+
+const getUserPlaylists = asyncHandler(async(req, res, next) => {
+
+})
+
+const getPlaylistById = asyncHandler(async(req, res, next) => {
+
+})
+
+const addVideoToPlaylist = asyncHandler(async(req, res, next) => {
+
+})
+
+
+const removeVideoFromPlaylist = asyncHandler(async(req, res, next) => {
+
+})
+
+const deletePlaylist = asyncHandler(async(req, res, next) => {
+
+})
+
+const updatePlaylist = asyncHandler(async( req, res, next) => {
+
+})
+
+
+export{
+    createPlaylist,
+    getUserPlaylists,
+    getPlaylistById,
+    addVideoToPlaylist,
+    removeVideoFromPlaylist,
+    deletePlaylist,
+    updatePlaylist
+}
+
