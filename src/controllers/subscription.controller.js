526afa44837bebab3e48dd0d21f257b8e9ba303f
Anshul Sharma <123247713+Anshul-Sharma01@users.noreply.github.com>
2024-08-27 17:50:36 +0530

    added subscription routes and controllers structure

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,27 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+const toggleSubscription = asyncHandler(async(req, res,next) => {
+
+})
+
+
+const getUserChannelSubscribers = asyncHandler(async(req, res, next) => {
+
+})
+
+
+const getSubscribedChannels = asyncHandler(async(req, res, next) => {
+
+})
+
+
+export {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+}
+
+
+
+
